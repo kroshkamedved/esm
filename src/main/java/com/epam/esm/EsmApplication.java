fdc4d1c778cf5414d25508abe70d9df821910321
kroshkamedved <kroshkamedved@gmail.com>
2023-08-13 14:32:33 +0300

    CI/CD done & tomcat config file added to readme

--- src/main/java/com/epam/esm/EsmApplication.java
+++ src/main/java/com/epam/esm/EsmApplication.java
@@ -3,10 +3,11 @@ package com.epam.esm;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class EsmApplication {
+public class EsmApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EsmApplication.class, args);
