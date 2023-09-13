25d605cf7f8422ff8e5218cdc3aca6e662dcfb06
kroshkamedved <kroshkamedved@gmail.com>
2023-09-13 22:58:39 +0300

    login page done

--- src/main/resources/javascript/login.js
+++ src/main/resources/javascript/login.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     var menuTrigger = document.getElementById('menu-checkbox');
     const menuList = document.getElementById('menu-list');
-// TODO checkbox reaction - show list:
+    menuTrigger.checked = false;
 
-    if(menuList.checked()){
-                menuList.style.display = 'flex';
-                menuList.style.flexDirection = "column";
-    }
+
     document.addEventListener('click', (event) => {
+            menuList.style.display = 'flex';
+            menuList.style.flexDirection = "column";
+
         // Check if the click target is outside the menuList
-        if (!menuList.contains(event.target) && event.target !== menuTrigger) {
+        if (!menuList.contains(event.target) && !menuTrigger.checked) {
             menuList.style.display = 'none';
         }
     });
